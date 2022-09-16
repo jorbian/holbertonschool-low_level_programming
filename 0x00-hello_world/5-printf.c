c71af2f294e87d10c27f80ddc003ae4b4ae91957
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-16 10:27:34 -0500

    Just when I said I was liking this...

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,14 @@
 #include <stdio.h>                                                                                                                                                                                                                                              
-/* This is a descriptor for the main function */
+/** 
+ * main - This is a descriptor of the main function 
+ * @void: I don't know why this is supposed to be neccessary but the
+ * style validator's word is law!! This is how cargo cult programming begins
+ * 
+ * return: 0 again, look up the definition of 'cargo cult programming' in the 
+ * Jargon file!!
+ */
 int main(void)                                                                                                                                                                                                                                                      
 {                                                                                                                                                                                                                                                               
-\tprintf("with proper grammar, but the outcome is a piece of art,\n");                                                                                                                     
-\treturn 0;                                                                                                                                                                                                     
+    printf("with proper grammar, but the outcome is a piece of art,\n");                                                                                                                     
+    return 0;                                                                                                                                                                                                     
 }
