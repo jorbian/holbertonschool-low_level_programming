16a759c93a91779a52022de99e92db61c423b5ae
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-16 19:28:08 -0500

    Take a deep breath and see if we can appease that fickle God the checker.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>                                                                                                                                                                                                                                            
+/** 
+ * main - This is a descriptor of the main function
+ * @void: I don't know why this is supposed to be neccessary but the
+ * style validator's word is law!! This is how cargo cult programming begins
+ * 
+ * Return: 0 again, look up the definition of 'cargo cult programming' in the 
+ * Jargon file!!
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
