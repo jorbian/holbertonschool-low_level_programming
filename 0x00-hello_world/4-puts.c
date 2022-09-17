258702b30644737c01971ea38487609b8fb404e8
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-16 20:26:50 -0500

    daFADFASDFASDFASD

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/** main - this is a description of the main function
+* @void main takes no arguyments. I have no idea why it would  ever actually
+* need to.
+* 
+* return: 0 I have no idea when or if main would ever return anything other
+* than 
+*/
+int main(void)
+{
+	puts("\x22Programming is like building a multilingual puzzle");
+
+	return (0);
+}
