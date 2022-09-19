45ede3a68185c04e48568dfe35301a1dd83dbc3e
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-19 13:27:41 -0500

    You just have to keep reapeating to yourself why you're here, Johnny boy, you just have to keep repeating to yourself why your'e here....

---
+++ 0x00-hello_world/z6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - I NEED TO COME BACK TO THIS LATERRRR!!!
+* @void: I can't imagine any of our projects taking arguments
+*
+* Return: 0 I can't imagine main ever returning anything...
+*/
+
+int main(void)
+{
+    return (0);
+}
