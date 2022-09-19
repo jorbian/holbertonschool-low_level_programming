c776e310a422e359ba95864ffddd2f7b2f8a5175
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-19 11:15:07 -0500

    They claim to solve many of the probblems of traditional school but so far 99% of the time what I've learned is how to appease people's arbitrary standards that have no bearing or relevance in the real world. Sounds alot like traditional fucking school to me!!!!!!!!!!!!!!!!!!

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 /**
-* main - The ritually descriptor 
+* main - The ritually descriptor
 * @void: I can't imagine any of our projects taking arguments
 *
 * Return: 0 I can't imagine main ever returning anything...
@@ -13,6 +14,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	if (n > 0)
+		printf("%d is positive", n);
+	}
+		else if (n < 0)
+	{
+		printf("%d is negative", n);
+	}
+	else
+	{
+		printf("%d is zero", n);
+	}
+
 	return (0);
 }
