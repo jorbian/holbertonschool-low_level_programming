ccb3027abda6b368a7754a91f17112628ae5fd14
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-19 10:28:35 -0500

    Somr questions for later.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main - this description serves no purpose.
+* main - I NEED TO COME BACK TO THIS LATERRRR!!!
 * @void: I can't imagine any of our projects taking arguments
 *
 * Return: 0 I can't imagine main ever returning anything...
@@ -8,6 +8,21 @@
 
 int main(void)
 {
-	printf("Hello world!\x0a");
+
+	char head[10] = "Size of a";
+	char foot[10] =  "byte(s)\x0a";
+	char data_TypE[5][18] = {
+		" char: %d",
+		"n int: %d",
+		" long int: %d",
+		" long long int: %d",
+		" float: %d"
+	};
+
+	for (int i = 0; i <= 5; i++) {
+		char with_size[255] = data_TypE[i], sizeof(char);
+		printf("%s%s%s", head, with_size, foot);
+	}
+
 	return (0);
 }
