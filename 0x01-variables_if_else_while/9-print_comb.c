9dab9cb5893d24852b27ef86e367c9f05edd32f0
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-19 20:12:49 -0500

    meh?

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,9 +9,18 @@ int main(void)
 {
 	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+		
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	
+	putchar('\x0a');
+	
 	return (0);
 }
-
