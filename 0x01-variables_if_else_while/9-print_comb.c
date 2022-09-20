c836b023d6a7879a6699222fbb9afc96fd514f34
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-19 20:13:36 -0500

    a'sofjk

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,15 +12,15 @@ int main(void)
 	for (n = 0; n < 10; n++)
 	{
 		putchar(n + '0');
-		
+
 		if (n != 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	
+
 	putchar('\x0a');
-	
+
 	return (0);
 }
