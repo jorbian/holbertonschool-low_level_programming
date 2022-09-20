e4d27c4a6c1967da490271c64158261a5651a917
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-20 10:39:04 -0500

    I was curious enough to want to run this one immediately.

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 /**
-* main - The ritually descriptor
-* @void: I can't imagine any of our projects taking arguments
+* print_to_98 - The ritually descriptor
+* @n: takes an i(n)teger. any i(n)teger you wantses
 *
-* Return: 0 I can't imagine main ever returning anything...
+* Return: It don't return nothing. 
 */
 
-int main(void)
+void print_to_98(int n);
 {
-	int n;
+	int i;
+	int direction;
 
-	return (0);
+	if (n > 98) {
+		direction = -1;
+	}
+	else 
+	{
+		direction = 1;
+	}
+
+	i = n;
+	do 
+	{
+		printf(" %d,", i);
+
+		i = i + direction;
+	} while (i != 98);
 }
