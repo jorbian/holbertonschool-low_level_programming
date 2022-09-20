f91d32c1d442214a4ab80a67608e7ac18f65f128
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-20 12:29:53 -0500

    ;ladjf;lakdhjf;kajsd;k'fja;'ldjf

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,14 +3,20 @@
 * print_sign - The ritually descriptor
 * @n: I can't imagine any of our projects taking arguments
 *
-Prototype: int print_sign(int n);
-Returns 1 and prints + if n is greater than zero
-Returns 0 and prints 0 if n is zero
+Returns the result
 */
 
 int print_sign(int n)
 {
-	if (n > 0) return 1;
-	if (n < 0) return -1;
+	char result[2];
+
+	if (n > 0)
+	{
+		result = "+1";
+	};
+	if (n < 0) 
+	{
+		result = "-1";
+	}
 	return 0;
 }
