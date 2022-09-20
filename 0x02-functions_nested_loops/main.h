bbf2e147d350f8bd3db8a31b3edc6cf3bee7addd
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-20 10:12:27 -0500

    I still have no idea just what the fuck is going on,

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,33 +9,8 @@
 * 0x02-functions_nested_loops directory.
 */
 
-#include <stdio.h>
-#include <unistd.h>
+int _putchar(char c);
+void display_message(void);
+void print_alphabet(void);
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
-* print_alphabet - function that prints the alphabet, in lowercase, followed
-* by a new line.
-*
-* Return: It's a void function and by definition does not return anything.
-*/
-void print_alphabet(void)
-{
-    int n;
-
-	for (n = 97; n <= 122; n++)
-		_putchar(n);
-	_putchar('\x0a');
-}
-
+#endif 
