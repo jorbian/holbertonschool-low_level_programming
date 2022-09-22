a93fc3f855d7806d1bcbda150a24e0cc33f21d9d
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-22 11:16:46 -0500

    as;fkl;

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * mul - check the code
+ * @a: a number
+ * @b: another number
+ *
+ * Return: the first number times the second number
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
