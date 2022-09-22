18abb1e2bc6f5fd0221955184de41c36bcdff3c8
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-22 11:57:44 -0500

    Coming along quite nicely this is.

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*  print_most_numbers - check the code
+*
+* Return: Is void and doesn't return anythinbg
+*/
+
+void print_most_numbers(void)
+{
+	int ch;
+
+	for (ch = '1'; ch <= '9'; ch++)
+	{
+		if ((ch != '2') && (ch != '4'))
+            _putchar(ch);
+	}
+	_putchar('\x0a');
+}
