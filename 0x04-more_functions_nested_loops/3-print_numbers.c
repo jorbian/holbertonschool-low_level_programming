7b0218c30a3f482d06ee3aa163de308ec0723647
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-22 11:51:27 -0500

    .kjha;dlkfhj

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
 /**
- *  print_numbers - check the code
- *
- * Return: Is void and doesn't return anythinbg
- */
+*  print_numbers - check the code
+*
+* Return: Is void and doesn't return anythinbg
+*/
 
 void print_numbers(void)
 {
+	char ch;
 
+	for (ch = '0'; ch < '9'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\x0a');
 }
