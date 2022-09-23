5242a0b54f397cc4d79295b9407dbcb6ff654069
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-23 10:54:15 -0500

    Another super confusing task. I thoughbt it worth a try to see if the way the task is described worked before I tried what the output looks like.

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*  print_diagonal - check the code
+* @n: an integer representing the number of times the character
+* is to be printed.
+*
+* Return: Is void and doesn't return anythinbg
+*/
+
+void print_diagonal(int n)
+{
+	char char_to_print = '\\';
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(char_to_print);
+	}
+	_putchar('\x0a');
+}
