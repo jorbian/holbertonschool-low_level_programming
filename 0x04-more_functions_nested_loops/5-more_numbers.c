ae3096d98dddacf4fa7695803c2968b7d4cc4eae
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-23 11:11:46 -0500

    asdf;hadsfh;adhjfljahdsf

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,10 +10,9 @@ void more_numbers(void)
 {
 	int i, j;
 
-
 	for (i = 1; i <= 10; i++)
 	{
-		for (j = 1; j <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
 				_putchar(j / 10 + '0');
@@ -22,4 +21,3 @@ void more_numbers(void)
 		_putchar('\x0a');
 	}
 }
-
