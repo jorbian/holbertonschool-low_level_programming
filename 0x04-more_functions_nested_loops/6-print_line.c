528c0a1ae84e3b8a37c29aa5b02c32e0b193af78
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-23 10:20:20 -0500

    ;ihj

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*  print_line - check the code
+* @n: an integer representing the number of times the character
+* is to be printed.
+*
+* Return: Is void and doesn't return anythinbg
+*/
+
+void print_line(int n)
+{
+    char char_to_print = '_';
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        _putchar(char_to_print);
+    }
+    _putchar('\x0a');
+}             
