8d2226a8eb092f57fecea8629991e92320d63463
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-26 10:15:42 -0500

    ejlqhFPOa;dhfiopkjasl;fhadsl;hfopaisdufiopadFUY

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * *_strcpy - a shameless ripoff of knrs verion of this function
+ * @dest: pointer to zeroeth character in destination
+ * @src: pointer to zeroeth character in source
+ *
+ * Return: void 
+ */
+
+char* _strcpy(char *dest, char *src)
+{
+    char unnecessary_retrurn_value = *dest;
+
+    while ((*src++ = *dest++) != '\0') 
+        ;
+    return (&unnecessary_retrurn_value);
+}
+// Just come back to it later...
