dde6ae60bbfbbcc90463fc51f45fc82f218e4703
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-26 15:44:08 -0500

    See my other adfsiej

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,8 +8,17 @@
 */
 void print_rev(char *s)
 {
+	int i, temp;
     char *pointer; 
-    rev_string(s);
+	int length = strlen(s);
+
+	for (i = 0; (i < length / 2); i++)
+	{
+		temp = s[i];
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = temp;
+	}
+
     pointer = s;
 
     while (*pointer++ != '\0') 
