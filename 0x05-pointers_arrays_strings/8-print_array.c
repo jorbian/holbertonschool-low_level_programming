1ecd85bc7e39a7945961411cb06f3f7009db1962
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-26 10:27:43 -0500

    Just trying to appease petty and spiteful gods

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,21 +3,21 @@
 /**
  * print_array - a shameless ripoff of knrs verion of this function
  * @a: pointer to zeroeth character in array
- * @n: number of elements to print out 
+ * @n: number of elements to print out
  *
  * Return: does not return anytrhing
  */
 
 void print_array(int *a, int n)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", *a);
-        a++;
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *a);
+		a++;
 
-        if (i != n)
-            printf(", ");
-    }
+		if (i != n)
+			printf(", ");
+		}
 }
