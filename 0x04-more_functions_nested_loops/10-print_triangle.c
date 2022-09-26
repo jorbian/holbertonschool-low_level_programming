1a536a9d391c67167b6726ae77c34486051a58b3
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-26 12:13:19 -0500

    a;ldjksfgkla;shjrfgopiasdhfopgj hwiopfghj

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * print_triangle - pojdfakjsodakhjf 
+  * @size: Tasdfghj asdfghjk asdfghj asdfghj wertghj sdfghj asdfg sdf sdfg sdf?
+  *
+  * Return: empty
+  */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\x0a');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = size - i; j > 1; j--)
+			{
+				_putchar(' ');
+			}
+
+			for (k = 0; k <= i; k++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\x0a');
+		}
+	}
+}
