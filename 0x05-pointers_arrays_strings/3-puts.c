b0e204d89f52a1744b8e6b87ef4832caaafb30d0
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-26 12:47:17 -0500

    ';ojkkc];'jvdfkls[zjgop]

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * _puts - Reverses a string
+  * @str: The string
+  *
+  * Return
+  *
+*/
+
+void _puts(char *str)
+{
+    while (str != '\0')
+    {
+        _putchar(str);
+        str++;
+    }
+}
+
