76a50befde46948784761f64709c11fee553f518
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-28 10:06:41 -0500

    asd';lj

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,11 +8,11 @@
  * Return: void 
  */
 
-char* _strcpy(char *dest, char *src)
+char _strcpy(char *dest, char *src)
 {
     char unnecessary_retrurn_value = *dest;
 
     while ((*src++ = *dest++) != '\0') 
         ;
-    return (*dest);
+    return (dest);
 }
