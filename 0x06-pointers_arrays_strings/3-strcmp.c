16f026fc0248b6885d925635ebb9abd7d08c4ac9
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-04 17:43:13 -0500

    adsf asdf adsf

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-* _strcmp - ads fadf sadsf safd 
-* @s1: asdf asdf adfs ks fdldd 
+* _strcmp - ads fadf sadsf safd
+* @s1: asdf asdf adfs ks fdldd
 * @s2: asdfghjk asdfghjkl wertyui
-* 
+*
 * Return: asdf asdf asdf adfs asdf asdf
 */
 int _strcmp(char *s1, char *s2)
@@ -17,4 +17,3 @@ int _strcmp(char *s1, char *s2)
 
 	return (*s1 - *s2);
 }
-
