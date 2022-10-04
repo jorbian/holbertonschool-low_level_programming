d18b8feb89f8001c0bdf914b5d84a9caf435b290
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-04 18:05:10 -0500

    adfs adsf asdf asdf asdf asdf asdf adsf asdf asdf asdf asdf asdf asdf

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* reverse_array - ads fadf sadsf safd
+* @a: asdf asdf adfs ks fdldd
+* @n: asdfghjk asdfghjkl wertyui
+*
+* Return: asdf asdf asdf adfs asdf asdf
+*/
+void reverse_array(int *a, int n)
+{
+
+}
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* *string_toupper - ads fadf sadsf safd
+* @a: asdf asdf adfs ks fdldd
+* @n: asdfghjk asdfghjkl wertyui
+*
+* Return: asdf asdf asdf adfs asdf asdf
+*/
+char *string_toupper(char *)
+{
+
+}
