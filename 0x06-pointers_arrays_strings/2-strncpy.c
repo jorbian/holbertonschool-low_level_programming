1aa361e4da781c4d53a8363eed4e2a502d2b9420
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-04 16:57:53 -0500

    So you're saying there's a chance?

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,7 +14,7 @@ char *_strncpy(char *dest, char *src, int n)
 	while (*(src + i++))
 		src_length++;
 
-	for (i = 0; (*(src + i) && i) < n; i++)
+	for (i = 0; *(src + i) && i < n; i++)
 		*(dest + i) = *(src + i);
 	for (i = src_length; i < n; i++)
 		*(dest + i) = '\0';
