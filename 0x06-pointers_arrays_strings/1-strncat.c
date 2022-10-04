7bfc5c7c3a9aeb90f9a5d0b6279a81077bf6ffc9
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-04 16:41:28 -0500

    Eh egh?

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/** *_strncat - a function to function function function
+* @dest: a variable
+* @src: another variable
+* @n: an integer
+* return: stuff
+*/
+char *_strncat(char *dest, char *src, in n)
+{
+	char *string;
+
+	return (string);
+}
