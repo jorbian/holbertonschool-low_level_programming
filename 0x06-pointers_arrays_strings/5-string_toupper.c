6cae7de6fb08a5e3f37bfc01b0c1976b6fd0e43b
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-04 19:22:27 -0500

    asdf asdf

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,12 +2,20 @@
 
 /**
 * *string_toupper - ads fadf sadsf safd
-* @a: asdf asdf adfs ks fdldd
-* @n: asdfghjk asdfghjkl wertyui
+* @str: the original stringd
 *
-* Return: asdf asdf asdf adfs asdf asdf
+* Return: the modified string
 */
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
+	int i = 0;
 
+	while (*(str + i))
+	{
+		if (*(str + i) >= '\x61' && *(str + i) <= '\x7a')
+			*(str + i) -= 32;
+		i++;
+	}
+
+	return (str);
 }
