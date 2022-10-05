59276719e3efdf8639ddb863fa27fcf367b4eeb0
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-05 14:00:35 -0500

    asdfo'jasd;kfj lasjdf

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - Fills the first n bytes of the memory area
+ * @s: Pointer to an area in memory
+ * @c: The character to fill the memory area with.
+ * @n: The number of bytes to be filled.
+ *
+ * Return: Pointer to area in memory @s.
+ */
+void *_memset(void *s, int c, size_t n)
+{
+	int i;
+	char *memory_area = s, value_to_use = c;
+
+	for (i = 0; i < n; i++)
+		*(memory_area + i) = value_to_use;
+
+	return (memory_area);
+}
