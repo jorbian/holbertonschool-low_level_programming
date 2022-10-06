32a0723a62431fa4baf0b287cb4b0dac5f6aeebc
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-06 13:09:40 -0500

    Get some of the ritual file creation out of the way

---
+++ 0x0B-argc_argv/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+* File: Main.h
+* Auth: Jonathan Boller
+* Desc: Header containing declarations for all functions used in the
+* 0x05-pointers_arrays_strings project thing
+*/
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
+#endif
