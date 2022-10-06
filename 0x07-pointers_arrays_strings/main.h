44ee570425f04e0a815a782f5ac79fb27c504e4f
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-05 23:31:40 -0500

    asdf

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,7 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *string, char *accept);
 char *_strpbrk(char *string, char *accepted_bytes);
-char *_strstr(char *haystack, char *needle);
+char *_strstr(char *source_string, char *sub_string);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 
