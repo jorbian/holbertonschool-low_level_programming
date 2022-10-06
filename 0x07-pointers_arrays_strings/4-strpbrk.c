ab866a63cee6d946c13474ee441adf12a0fd5d92
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-05 23:15:05 -0500

    Again just smashing buttons until it likes it

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,14 +9,15 @@
 char *_strpbrk(char *string, char *accepted_bytes)
 {
 	int address1 = 0;
-	int address2 = 0;
+	int address2;
 
 	while (*(string + address1))
 	{
+		address2 = 0;
+		
 		while (*(accepted_bytes + address2))
 		{
-			if (*(string + address1) ==
-			*(accepted_bytes + address2))
+			if (string[address1] == accepted_bytes[address2])
 			{
 				string += address1;
 				return (string);
