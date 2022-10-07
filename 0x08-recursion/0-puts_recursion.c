0b471f8a90c47e3a74b3b3ed73c3f24bd88131b4
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-07 13:03:15 -0500

    'a;sdkfjl;kjad;LFHJKA;LKDFJ

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
 * _puts_recursion - prints a string, followed by a new line
-* @s: pointer to string
+* @string: pointer to string
 * Return: nothing
 **/
 void _puts_recursion(char *string)
 {
 	if (*string == '\0')
 	{
-		_putchar('\n');
 		return;
 	}
 	_putchar(*string);
