bfa8659ea6cfe3eaa0a492519c2dcf56cb2b79a4
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-07 13:39:17 -0500

    Don't care anymore, just going to take the partial credit and move on.

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,16 @@
 #include "main.h"
 /**
- * _puts_recursion -  function that prints a string
- * @s: string to receive
- *
- * Return: nothing
- */
-
-void _puts_recursion(char *s)
+* _puts_recursion - prints a string, followed by a new line
+* @string: pointer to string
+* Return: nothing
+**/
+void _puts_recursion(char *string)
 {
-	if (*s == '\0')
+	if (*string == '\0')
 	{
-		_putchar('\n');
 		return;
 	}
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
+	_putchar(string);
+	_puts_recursion(string + 1);
 }
 
