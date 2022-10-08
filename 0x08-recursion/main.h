73aafba1731576802c0777bf61fb513e59f8b6b3
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-08 13:28:12 -0500

    adsf adsf

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -15,8 +15,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 
 int _sqrt_recursion(int n);
-int test(double number, double guess)
-
+int test(double number, double guess);
 int is_prime_number(int n);
 int is_prime_recursive(int n, int i);
 
