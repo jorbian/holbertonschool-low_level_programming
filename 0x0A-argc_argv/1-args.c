01657099a196d2f1a63b6f2e7a7c05afb23207d6
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-08 19:29:04 -0500

    adfsf adsf

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - ads f
+ * @argc: number of arguments
+ *
+ * Return: zero
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
