e8e2f0660381f64075f743ec56433bf27b11c98b
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-08 20:45:39 -0500

    Pushing version for parial crdit before fixing it.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - ads f
  * @argc: number of arguments
@@ -8,8 +9,26 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, accum;
+	int i, total, current_number;
 
+	total = 0;
+	current_number = 0;
 	for (i = 0; i < argc; i++)
+	{
+		current_number = atoi(argv[i]);
 
+		if (current_number)
+		{
+			total = total + current_number;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	printf("%d", total);
+
+	return (0);
 }
