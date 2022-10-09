23e8a14779dc6ef96501506649acd8223e4775b5
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-08 20:07:09 -0500

    This is actually coming along quite nicely.

---
+++ 0x0A-argc_argv/ 3-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - ads f
+ * @argc: number of arguments
+ * @argv: asdf asdf
+ *
+ * Return: zero
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", (argc - 1));
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - ads f
+ * @argc: number of arguments
+ * @argv: asdf asdf
+ *
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
