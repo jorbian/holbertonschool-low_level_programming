c0a96f3bc7607cdcd9c4c1644f5aa75e2de97357
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-08 19:09:30 -0500

    asdf adsf f

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - ads f
+ * @argc: number of arguments
+ * @argv: arguments
+ * 
+ * Return: zero
+ */
+int main(int argc, char **argv)
+{
+	printf("%s", argv[0]);
+}
