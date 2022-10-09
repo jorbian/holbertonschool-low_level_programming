73a964bf65f5fd934f38b626e2799ae2331351c4
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-08 20:21:18 -0500

    This was totally the most strightforward thing I've ever done!!

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - ads f
+ * @argc: number of arguments
+ * @argv: asdf asdf
+ *
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		int x = atoi(argv[1]);
+		int y = atoi(argv[2]);
+		
+		printf("%d\n", (x * y));
+
+		return (0);
+	}
+}
