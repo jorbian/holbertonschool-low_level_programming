5b8dc9701aa84dca51493cab87f0dcdc41a385b6
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-11 16:14:43 -0500

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 /**
  * _strdup -  a function that returns a pointer to a newly allocated space in
  * memory, which contains a copy of the string given as a parameter.
- * @str: the string to be copied into newly allocated space.
+ * @string: the string to be copied into newly allocated space.
  *
  * Return: a pointer to the first byte of the copy of the given string.
  */
@@ -14,14 +14,13 @@ char *_strdup(char *string)
 	if (!(string))
 		return (NULL);
 
-    i = 0;
-    while (string[i] != '\0')
+	i = 0;
+	while (string[i] != '\0')
 		i++;
-    i++;
+	i++;
 
-    *copy_of_string = malloc(i * sizeof(char));
-    for (j = 0; j < i; j++)
+	*copy_of_string = malloc(i * sizeof(char));
+	for (j = 0; j < i; j++)
 		copy_of_string[j] = string[j];
-
 	return (copy_of_string);
 }
