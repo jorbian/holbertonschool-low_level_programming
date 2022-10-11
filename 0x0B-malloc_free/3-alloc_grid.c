9399ca4296f0a950d3af871dc28859f53baad4e9
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-11 16:46:30 -0500

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * alloc_grid - function that returns a pointer to a 2 dimensional array of
  * integers.
@@ -9,5 +10,31 @@
  */
 int **alloc_grid(int width, int height)
 {
+	int i, j;
+	int **pointer;
 
+	i = j = 0;
+	if (height < 1)
+		return (NULL);
+	pointer = (int **)malloc(height * sizeof(pointer));
+
+	if (!(pointer == NULL))
+	{
+		free(pointer);
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		pointer[i] = malloc(width * sizeof(int));
+		if (!(pointer[i]))
+		{
+			for (j = 0; j < i; j++)
+				free(pointer[j]);
+			free(pointer);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			pointer[i][j] = 0;
+	}
 }
