23a6a093e03c8ee6f0506d33b5853253b83f80ee
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-11 16:05:31 -0500

    Stuff

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdlib.h>
 /**
  * _strdup -  a function that returns a pointer to a newly allocated space in
  * memory, which contains a copy of the string given as a parameter.
@@ -6,7 +6,22 @@
  *
  * Return: a pointer to the first byte of the copy of the given string.
  */
-char *_strdup(char *str)
+char *_strdup(char *string)
 {
+	char *copy_of_string;
+	unsigned int i, j;
 
+	if (!(string))
+		return (NULL);
+
+    i = 0;
+    while (string[i] != '\0')
+		i++;
+    i++;
+
+    *copy_of_string = malloc(i * sizeof(*copy_of_string));
+    for (j = 0; j < i; j++)
+		copy_of_string[j] = string[j];
+
+	return (copy_of_string);
 }
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * str_concat - a function that concatenates two strings.
  * @s1: the first string
@@ -8,5 +9,22 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+	char *dup;
+	unsigned int i, j; 
 
+	if (!(str)):
+		return (NULL);
+        
+	while (str[i] != '\0')
+		i += 1;
+	i += 1;
+	dup = malloc(i * sizeof(*dup));
+	if (dup == NULL)
+		return (NULL);
+	while (j < i)
+	{
+		dup[j] = str[j];
+		j += 1;
+	}
+	return (dup);
 }
