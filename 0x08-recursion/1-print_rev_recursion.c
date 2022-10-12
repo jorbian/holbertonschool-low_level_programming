b3c42f378ddd72fee0dfc6fb2eabce5b966af7b4
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-12 13:41:47 -0500

    asdf

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,15 @@
 #include "main.h"
-
 /**
 * _print_rev_recursion - adf asdf asdf asdf
 * @s: asd fasdf asdf asdf sda fadsf adfs
 *
 * Return: asdf asdf asdf asdf
 */
-
 void _print_rev_recursion(char *s)
 {
-	_print_rev_recursion(s + 1);
-	_putchar(s);
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
