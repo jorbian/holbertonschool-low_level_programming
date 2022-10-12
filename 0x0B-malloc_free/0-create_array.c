18c0cb736e158d3f92f61a6dc6a4890887cbac44
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-12 14:03:15 -0500

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,14 +18,11 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 
 	array = malloc(size * sizeof(char));
-
-	if (array)
-	{
-		for (i = 0; i < size; i++)
-			array[i] = c;
-	}
-	else
+	if (!(array))
 		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
 
 	return (array);
 }
