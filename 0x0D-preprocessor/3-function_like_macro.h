d56b229709023453fc2ffc8df6bdfafbac19c1eb
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-18 13:15:14 -0500

    asdf adsf adfs asd asdf

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,7 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(n) ((n) * ((n>0) - (n<0)))
+//#define ABS(n) ((n) * ((n>0) - (n<0)))
+#define ABS(n) ((n) < 0 ? (-n) : (n))
 
 #endif
