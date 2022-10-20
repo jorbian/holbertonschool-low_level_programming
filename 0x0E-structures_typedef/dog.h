01e10bac369a90e76ea4f72f0198ff9a2a709ace
jorbian <5533@holbertonstudents.com>
2022-10-20 04:59:00 -0500

    Lets try this again now that betty is happy.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,16 +2,16 @@
 #define DOG_H
 
 /**
- * dog - Short description
+ * struct dog - asdf asdf adsf
  * @name: a pointer to the first character of dog's name
- * @float: age of the dog expressed as floating point 
+ * @age: age of the dog expressed as floating point
  * @owner: a pointer to the first character of owner's name
  *
  * Description: Longer description
  */
 struct dog
 {
-	char *name; 
+	char *name;
 	float age;
 	char *owner;
 };
