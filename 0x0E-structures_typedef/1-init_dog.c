861c56c5e78d96474c241a636c25872cbd17c5a8
jorbian <5533@holbertonstudents.com>
2022-10-20 05:13:50 -0500

    See what happens

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - constructor method for the object dog
+ * @d: pointer to where we're going to put the parameters
+ * @name: where we're going to put its name
+ * @age: the dogs age as a float
+ * @owner: pointer to where we're going to not its owner.
+ *
+ * Return: just returns void
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
