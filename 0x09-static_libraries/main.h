30f2bb1e11e7ecdfcd16ec7ea69d1a73627ebc69
jorbian <5533@holbertonstudents.com>
2022-10-20 04:49:48 -0500

    Some inital commits

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * dog - Short description
+ * @name: a pointer to the first character of dog's name
+ * @float: age of the dog expressed as floating point 
+ * @owner: a pointer to the first character of owner's name
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name; 
+	float age;
+	char *owner;
+};
+
+#endif
