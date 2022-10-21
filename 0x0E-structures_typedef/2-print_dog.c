ce653188552b783ed26822c06630842724fd3715
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-21 18:02:31 -0500

    asdf asdf

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,22 +11,22 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		printf("Name:");
-		if (d->name)
-			printf(" %s\x0a", d->name);
-		else
+		if (!d->name)
 			printf(" (nil)\x0a");
+		else
+			printf(" %s\x0a", d->name);
 
 		printf("Age:");
-		if (d->age != d->age)
-			printf(" %f\x0a", d->age);
-		else
+		if (!d->age)
 			printf(" (nil)\x0a");
+		else
+			printf(" %f\x0a", d->age);
 
 		printf("Owner:");
-		if (d->owner)
-			printf(" %s\x0a", d->owner);
-		else
+		if (!d->owner)
 			printf(" (nil)\x0a");
+		else
+			printf(" %s\x0a", d->owner);
 	}
 	else
 		return;
