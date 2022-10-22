8da0095237bdbb780deaeb6a82d86024dad53eed
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-21 19:15:33 -0500

    asdf adf

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * new_dog - constructor method for the object dog
@@ -10,11 +11,33 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t new_dog;
+	dog_t *new_dog;
+	int i;
 
-	new_dog->name = name;
+	int name_length = 0;
+	int owner_length = 0;
+	while(name[name_length++])
+		;
+	while(owner[owner_length++])
+		;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (!new_dog)
+		return (NULL);
+
+	new_dog->name = malloc(name_length * sizeof(new_dog->name));
+	if (!new_dog)
+		return (NULL);
+	for (i = 0; i < name_length; i++)
+		new_dog->name[i] = name[i];
+	
 	new_dog->age = age;
-	new_dog->owner = owner;
 
-	return (*new_dog);
+	new_dog->owner = malloc(owner_length * sizeof(new_dog->owner));
+        if (!new_dog)
+                return (NULL);
+        for (i = 0; i < owner_length; i++)
+                new_dog->owner[i] = owner[i];
+
+	return (new_dog);
 }
