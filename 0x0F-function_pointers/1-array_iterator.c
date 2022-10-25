6893c2593e41e18cddbd5beffef088cfa83547a2
jorbian <5533@holbertonstudents.com>
2022-10-24 21:27:06 -0500

    This should actually work,

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+* array_iterator - asdf afds afd addddff
+* @array: an array
+* @size: size of the array in bytes
+* @action: a pointer to whatever it is you're doing to the array
+*
+* Return: does not return anything
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (!array || !size)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
@@ -5,5 +7,6 @@ int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
