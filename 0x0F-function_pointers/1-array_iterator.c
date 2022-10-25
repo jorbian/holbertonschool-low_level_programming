24dcf507f5ca4d62732e40a6ee5cf28f74792f14
jorbian <5533@holbertonstudents.com>
2022-10-24 22:10:10 -0500

    asdf afds adsf

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (!array || !size)
+	if (!array || !size || !action)
 		return;
 
 	for (i = 0; i < size; i++)
