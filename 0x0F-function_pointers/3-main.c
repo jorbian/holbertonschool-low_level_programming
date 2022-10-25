fe77b3f77326373138f4f2f475305a9477acb6ec
jorbian <5533@holbertonstudents.com>
2022-10-25 00:12:44 -0500

    See if it passes if the checker doesn't care I don't care

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "function_pointers.h"
+
+/**
+* main - simple calculator programme to demonstrate the use of func pointerss
+* @argc: number of arguments fed to program
+* @argv: array containing
+*
+* Return: asdf asd fasd faasdf
+*/
+int main(int argc, char *argv[])
+{
+	int first_term, second_term;
+	int (*operation)(int, int);
+
+	first_term = atoi(argv[1]);
+	second_term = atoi(argv[3]);
+	operation = get_op_func(argv[2]);
+
+	if ((argc != 4) || (argv[2][1]) || !operation)
+	{
+		printf("Error\x0a");
+		return (0);
+	}
+
+	printf("%d\x0a", operation(first_term, second_term));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "function_pointers.h"
+
+/**
+* op_add - function to add two integers together
+* @a: the first integer
+* @b: the second integer
+*
+* Return: the sum of the two integers
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - function to subtract one integer from another
+* @a: the first integer
+* @b: the second integer
+*
+* Return: the difference of the two integers
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - function to multiply two integers together
+* @a: the first integer
+* @b: the second integer
+*
+* Return: the product of the two integers
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - function to divide two integers
+* @a: the first integer
+* @b: the second integer
+*
+* Return: the quotient
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - asdf asdf asdf asdf asdf asdf
+* @a: the first integer
+* @b: the second integer
+*
+* Return: the sum of the two integers
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
