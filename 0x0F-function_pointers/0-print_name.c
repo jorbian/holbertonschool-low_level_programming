73c2f4de2fea4c81357417a561912dd2ffdb1c33
jorbian <5533@holbertonstudents.com>
2022-10-24 20:47:36 -0500

    Let's try this again...

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,12 @@
-#include <stdio.h>
 #include "function_pointers.h"
 
+/**
+* print_name - a function to print a name either upper or lowercase
+* @name: the name
+* @f: a pointer to the function to determine how to print it
+*
+* Return: doesn't return anything
+*/
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
