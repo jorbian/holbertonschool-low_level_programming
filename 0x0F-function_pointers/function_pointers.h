46d66a64a710504da892ed9094b02ec4d65d9ccd
jorbian <5533@holbertonstudents.com>
2022-10-24 20:49:55 -0500

    asdf asfd

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,6 +4,6 @@
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
-void print_name_uppercase(char *name)
+void print_name_uppercase(char *name);
 
 #endif
