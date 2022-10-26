1e79f1deef8a4f7d5410d84f495304dc6e1f3bbf
jorbian <60049930+jorbian@users.noreply.github.com>
2022-10-25 22:03:53 -0500

    asdf

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,20 +12,43 @@
 */
 int main(int argc, char *argv[])
 {
-	int first_term, second_term;
-	int (*operation)(int, int);
+	int first_term, second_term, answer;
+	int exit_code;
+	int (*res)(int, int);
+	char *get_op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
 	first_term = atoi(argv[1]);
 	second_term = atoi(argv[3]);
-	operation = get_op_func(argv[2]);
+	get_op = argv[2];
 
-	if ((argc != 4) || (argv[2][1]) || !operation)
+	if (!get_op_func(argv[2]) || argv[2][1])
 	{
-		printf("Error\x0a");
-		exit(99);
+		exit_code = 99;
+	}
+	else if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
+	{
+		exit_code = 100;
+	}
+	else 
+	{
+		exit_code = 0;
 	}
 
-	printf("%d\x0a", operation(first_term, second_term));
+	if (exit_code)
+	{
+		printf("Error\x0a");
+		exit(exit_code);
+	}
 
+	res = get_op_func(get_op);
+	answer = res(first_term, second_term);
+
+	printf("%d\n", answer);
 	return (0);
 }
