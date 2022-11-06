d4936197bec27fe1cd7e426a0e536c10bcf1240b
jorbian <60049930+jorbian@users.noreply.github.com>
2022-11-06 15:13:06 -0600

    Inital upload of list_len function

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,20 @@
 #include "lists.h"
-
+/**
+* list_len - adsf asdf asdf asdf
+* @h: asdfasdfasdf asdfasdf
+*
+* Return: adsf asdfasdfasdfasdf add
+*/
 size_t list_len(const list_t *h)
 {
+	size_t num_of_elements = 0;
 
+	while (h)
+	{
+		h = h->next;
+		num_of_elements++;
+	}
+
+	return (num_of_elements);
 }
+
