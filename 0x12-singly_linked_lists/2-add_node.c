5865a43fcad6405658b5b7d24e0882e60a25eda2
jorbian <60049930+jorbian@users.noreply.github.com>
2022-11-06 15:34:43 -0600

    adafds

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+* add_node - add a node to a singly linked list
+* @head: double pointer to the thing at the head of the list
+* @string: string to be added to list.
+*
+* Return: Null if function fails but otherwise new element
+*/
+list_t *add_node(list_t **head, const char *string)
+{
+	char *duplicate;
+	int length_of_string;
+	list_t *new_element;
+
+	*new_element = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	duplicate = strdup(string);
+	if (!duplicate)
+	{
+		free(new_element);
+		return (NULL);
+	}
+
+	for (length_of_string = 0; string[length_of_string];)
+		length_of_string;
+
+	new_element->string = duplicate;
+        new_element->length = length;
+        new_element->next = *head;
+
+	*head = new_element;
+
+	return (new_element);
+}
+
