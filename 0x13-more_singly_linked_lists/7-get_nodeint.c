20486d49ce8753e06713bc8ce55da2cf7fffc406
jorbian <60049930+jorbian@users.noreply.github.com>
2022-11-08 22:42:32 -0600

    See what happens.

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+* get_nodeint_at_index - prints node index
+* @head: node
+* @index: index of node we want to print
+*
+* Return: asdf
+*/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *temp;
+
+	temp = head;
+
+	while (i != index)
+	{
+		if (temp->next)
+		{
+			temp = temp->next;
+			i++;
+		}
+		else
+			return (NULL)
+	}
+
+	return (temp);
+}
