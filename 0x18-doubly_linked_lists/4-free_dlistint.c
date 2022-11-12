b6ac3cb3f5ce650dc303b090b3132b69c123e3a5
jorbian <5533@holbertonstudents.com>
2022-11-12 16:31:08 -0600

    Inital upload of fifth task

---
+++ 0x18-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* free_dlistint - free a doubly linked list
+* @head: first node in the list
+*
+* Return: void
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *placeholder;
+
+	while (head)
+	{
+		placeholder = head->next;
+		free(head);
+		head = placeholder
+	}
+}
+
