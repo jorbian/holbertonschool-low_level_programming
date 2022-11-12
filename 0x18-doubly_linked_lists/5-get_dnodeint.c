96aececef50bc4f090cff53b58583c4d8a806fed
jorbian <5533@holbertonstudents.com>
2022-11-12 17:03:01 -0600

    betty

--- 0x18-doubly_linked_lists/5-get_dnodeint.c
+++ 0x18-doubly_linked_lists/5-get_dnodeint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
-/** 
+/**
  * get_card_at_index - exactly what it says on the tin
  * @head: card on the top of the deck
  * @index: the index we're interest in grabbing
- * 
+ *
  * Return: the card we wanted
 */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
