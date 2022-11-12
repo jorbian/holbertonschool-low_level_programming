b136f0d3ce5b487cb4ce0e8632a89de2ea2a931e
jorbian <5533@holbertonstudents.com>
2022-11-12 16:07:52 -0600

    Inital upload of forth task

---
+++ 0x18-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+* add_dnodeint_end - add node at the end of doubly linked list
+* @head: head of the list
+* @n: number to be added
+*
+* Return: address of new element
+*/
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	distint_t *new, *last;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (!*head)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+	last = *head;
+
+	while (last->next)
+		last = last->next;
+
+	last->next = new;
+	new->prev = last;
+
+	return (new);
+}
+
