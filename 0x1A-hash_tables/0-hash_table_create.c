41d23e03a0ff849d3a158b98d68450bdd41d2648
jorbian <60049930+jorbian@users.noreply.github.com>
2022-11-14 16:04:45 -0600

    Fixed betty and fleshed out memory allocation.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,12 +8,19 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
 	hash_table_t *new_hash_table = malloc(size);
+	
 	if (!new_hash_table)
 		return (NULL);
 
 	new_hash_table->size = size;
-	new_hash_table->array = NULL;
+	new_hash_table->array = malloc(sizeof(hash_table_t *) * size);
+	if (!new_hash_table->array)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		new_hash_table->array[i] = NULL;
 
 	return (new_hash_table);
 }
