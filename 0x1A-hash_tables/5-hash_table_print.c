e49d1afdfb5fb4f69aff69b07128550f9d6c0903
jorbian <60049930+jorbian@users.noreply.github.com>
2022-11-16 13:03:33 -0600

    sixth task done

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_print - prints a hash table
+* @ht: the hash table to be printed
+*
+* Return: void.
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, flag = 0;
+	hash_node_t *current_node;
+
+	if (!ht)
+		return;
+
+	putchar("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (current_node)
+		{
+			while (current_node)
+			{
+				if (flag)
+					printf(", ");
+				printf("'%s': '%s'", current->key, current->value);
+				flag = 1;
+				current = current->next;
+			}
+		}
+	}
+	
+	putchar("}");
+	putchar("\x0a");
+}
+
