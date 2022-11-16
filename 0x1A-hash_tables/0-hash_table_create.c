1b0e0bbadf2dd73fa3f0449c11fb083da972ec75
jorbian <60049930+jorbian@users.noreply.github.com>
2022-11-16 11:24:09 -0600

    Meh?

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,11 +10,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 
-	hash_table_t *new_hash_table = malloc(size);
-	hash_table_t *data = malloc(sizeof(hash_table_t *) * size);
+	hash_table_t *new_hash_table;
+	hash_table_t *data;
+
+	new_hash_table = malloc(size);
+	data = malloc(sizeof(hash_table_t *) * size);
 
 	if ((!new_hash_table) || (!data))
 		return (NULL);
+
+	for (i = 0; i < size; i++)
+		data[i] = NULL;
 
 	new_hash_table->size = size;
 	new_hash_table->array = data;
